cb102f5123bb1ac1c38a4b6d6638042f3c31a701
Conor O'Brien <conorobrien4god@gmail.com>
2015-11-19 00:30:26 -0500

    added basic framework (incomplete)

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,1 +1,21 @@
+function Comm(code){
+  this.code       = code;
+  this.index      = 0;
+  this.stack      = [];
+  this.inputStack = [];
+  this.commands   = { // wip
+    " ": function(C){return;}
+    "\"": function(C){C.mode=1;}
+  }
+}
 
+Comm.prototype.read = function(input){
+  // input is given seperated by newlines
+  input = input.split("\n").map(eval);
+  // UNSAFE: FIND ALTERNATIVE ===^
+  this.inputStack = this.inputStack.concat(input);
+}
+
+Comm.prototype.step = function(){
+  var curChar = this.code[this.index];
+}

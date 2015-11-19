2ca0708947ef966752fb5f6a04a6efdd5047b856
Conor O'Brien <conorobrien4god@gmail.com>
2015-11-19 13:28:27 -0500

    Update interpreter.js

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,11 +1,14 @@
 function Comm(code){
-  this.code       = code;
-  this.index      = 0;
-  this.stack      = [];
-  this.inputStack = [];
-  this.commands   = { // wip
-    " ": function(C){return;}
-    "\"": function(C){C.mode=1;}
+  this.code                = code;
+  this.index               = 0;
+  this.paradigm            = "stack";
+  this.stack               = [];
+  this.inputStack          = [];
+  this.paraCmds            = {};
+  this.paraCmds.commands   = { // wip
+    " ": function(C){return;},
+    "\"": function(C){C.mode=1;},
+    "f": function(C){C.paradigm = "function"}
   }
 }
 
